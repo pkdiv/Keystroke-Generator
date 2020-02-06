6e7b904759aafbe64c9405d1a827f1fd48c067e8
Divyesh P K <50244658+pkdiv@users.noreply.github.com>
2020-02-06 20:20:27 +0530

    Add files via upload

---
+++ Keystroke.py
@@ -0,0 +1,40 @@
+from pynput.keyboard import Controller,Key,KeyCode
+import time
+keyboard = Controller()
+
+
+with open("txtfile.txt","r") as f:
+    contents = f.read()
+time.sleep(6)
+characters = r'qwertyuiopasdfghjklzxcvbnm,./;{}1234567890!@#$%^&*()<>"  '
+
+
+for char in contents:
+    try:
+        if char == "\n":
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+        elif char == "\t":
+            pass
+            # keyboard.press(Key.space)
+            # keyboard.release(Key.space)
+            # keyboard.press(Key.space)
+            # keyboard.release(Key.space)
+            # keyboard.press(Key.space)
+            # keyboard.release(Key.space)
+            # keyboard.press(Key.space)
+            # keyboard.release(Key.space)
+        else:
+            keyboard.press(char)
+            keyboard.release(char)
+        time.sleep(0.1)
+    except:
+        pass
+    # elif char == " ":
+    #     keyboard.press(Key.space)
+    #     keyboard.release(Key.space)
+    # else:
+    #     keyboard.press(KeyCode.from_char(char))
+    #     keyboard.release(KeyCode.from_char(char))
+
+
