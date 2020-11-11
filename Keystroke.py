fb47f5a25757dab7eab562197ede58e2c254f4b7
Divyesh P K <50244658+DebuggingWarrior@users.noreply.github.com>
2020-11-11 10:37:27 +0530

    Update keystroke.py for CLI arguments

--- Keystroke.py
+++ Keystroke.py
@@ -1,23 +1,46 @@
-from pynput.keyboard import Controller, Key, KeyCode
+from pynput.keyboard import Controller, Key
 import time
+import argparse
+import os
 
-keyboard = Controller()
 
-contents = input("Enter the text :")
+def arguments():
+    parser = argparse.ArgumentParser()
+    requiredNamed = parser.add_argument_group('required named arguments')
 
-time.sleep(6)
-characters = r'qwertyuiopasdfghjklzxcvbnm,./;{}1234567890!@#$%^&*()<>"  '
+    requiredNamed.add_argument("-f", "--file", help="Path to text file", dest="file", required=True)
+    parser.add_argument("-t", "--time", help="Time to wait before generating keystrokes (in secs)", type=int, dest="time", default=5)
+    return parser.parse_args()
 
-for char in contents:
-    try:
-        if char == "\n":
-            keyboard.press(Key.enter)
-            keyboard.release(Key.enter)
-        elif char == "\t":
+
+def keystroke(contents):
+    keyboard = Controller()
+    for char in contents:
+        try:
+            if char == "\n":
+                keyboard.press(Key.enter)
+                keyboard.release(Key.enter)
+            elif char == "\t":
+                pass
+            else:
+                keyboard.press(char)
+                keyboard.release(char)
+            time.sleep(0.1)
+        except:
             pass
-        else:
-            keyboard.press(char)
-            keyboard.release(char)
-        time.sleep(0.1)
-    except:
-        pass
+
+
+if __name__ == '__main__':
+    args = arguments()
+    if not (os.path.isfile(args.file)):
+        print("Invalid file path")
+
+    for secs in range(args.time, 0, -1):
+        print(f"\rStarting keystrokes in {secs} seconds", end="")
+        time.sleep(1)
+    print("\rKeystrokes started" + " "*20)
+
+    with open(args.file, "r") as inp_file:
+        file_text = inp_file.read()
+        keystroke(file_text)
+    print("Done")
